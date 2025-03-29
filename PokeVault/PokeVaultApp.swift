bd76bf9ab0f83cc1c8a9a6fbbc9e6700ef79248d
Ayman Tauhid <aymantauhid2@gmail.com>
2025-03-28 22:30:40 -0400

    Initial Commit

---
+++ PokeVault/PokeVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeVaultApp.swift
+//  PokeVault
+//
+//  Created by Ayman Tauhid on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct PokeVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
